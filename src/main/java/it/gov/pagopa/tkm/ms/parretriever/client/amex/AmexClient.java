1c903afc5fc4ad161a969c7973ed7e1989c2a12a
andrea rovere <e-aarv@gft.com>
2021-06-01 10:42:45 +0200

    Pagopa-943 test Client amex

--- src/main/java/it/gov/pagopa/tkm/ms/parretriever/client/amex/AmexClient.java
+++ src/main/java/it/gov/pagopa/tkm/ms/parretriever/client/amex/AmexClient.java
@@ -30,9 +30,11 @@ public class AmexClient {
 
     public String getPar(String pan) throws IOException {
         Properties properties = new Properties();
-        properties.put("CLIENT_KEY", clientId);
-        properties.put("CLIENT_SECRET", clientSecret);
-        properties.put("RETRIEVE_PAR_URL", RETRIEVE_PAR_URL);
+      //  properties.put("CLIENT_KEY", clientId);
+        properties.put("CLIENT_KEY", "??");
+        //properties.put("CLIENT_SECRET", clientSecret);
+        properties.put("CLIENT_SECRET", "??");
+         properties.put("RETRIEVE_PAR_URL", RETRIEVE_PAR_URL);
         PropertiesConfigurationProvider configurationProvider = new PropertiesConfigurationProvider();
         configurationProvider.setProperties(properties);
 
@@ -46,7 +48,9 @@ public class AmexClient {
             throw new IOException("HttpUrl not parsable");
         }
 
-        String amexParRequest = mapper.writeValueAsString(new AmexParRequest(pan));
+     //   String amexParRequest = mapper.writeValueAsString(new AmexParRequest(pan));
+      //  System.out.println("AMEX PAR REQUEST: " + amexParRequest);
+        String amexParRequest = "[\"374313476701813\"]";
         Map<String, String> headers = authProvider.generateAuthHeaders(amexParRequest, url, "POST");
         RequestBody body = RequestBody.create(amexParRequest, MediaType.parse("application/json; charset=utf-8"));
         Request.Builder builder = new Request.Builder()
@@ -58,13 +62,60 @@ public class AmexClient {
         Request request = builder.build();
         OkHttpClient httpClient = new OkHttpClient.Builder().build();
         Response response = httpClient.newCall(request).execute();
-        ResponseBody responseBody = response.body();
+       // ResponseBody responseBody = response.body();
+        String responseBodyString = response.body().string();
+        System.out.println(responseBodyString);
 
-        if (responseBody != null) {
-            AmexParResponse amexParResponse = mapper.readValue(responseBody.string(), AmexParResponse.class);
+        String fakeResponse = "{\"input\":\"374313476701813\",\"par\":\"A000FWD79TSY53DF135Z9H3TESTE2\",\"success\":true}";
+        if (responseBodyString != null) {
+            AmexParResponse amexParResponse = mapper.readValue(fakeResponse, AmexParResponse.class);
             return amexParResponse.getPar();
         }
         return null;
     }
+
+
+    public static void main (String... args) throws IOException {
+        retrievePar("[\"374313476701813\"]");
+    }
+
+
+    public static String retrievePar(String searchPayload) throws IOException {
+        Properties properties = new Properties();
+        properties.put("CLIENT_KEY", "PfsU4JWgDJZ4mJu9GgKqFRloIBZ7I3DT");
+        properties.put("CLIENT_SECRET", "vF9ABaTV5pG7m92U83EJj5roYBkwAqQJ");
+        properties.put("RETRIEVE_PAR_URL", RETRIEVE_PAR_URL);
+        PropertiesConfigurationProvider configurationProvider = new PropertiesConfigurationProvider();
+        configurationProvider.setProperties(properties);
+
+        AuthProvider authProvider = HmacAuthBuilder.getBuilder()
+                .setConfiguration(configurationProvider)
+                .build();
+
+        String url = configurationProvider.getValue("RETRIEVE_PAR_URL");
+
+        // This generates the AMEX specific authentication headers needed for this API.
+        Map<String, String> headers = authProvider.generateAuthHeaders(searchPayload, url, "POST");
+
+        RequestBody body = RequestBody.create(searchPayload, MediaType.parse("application/json; charset=utf-8"));
+        Request.Builder builder = new Request.Builder()
+                .url(url)
+                .post(body);
+
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            builder.addHeader(header.getKey(), header.getValue());
+        }
+        Request request = builder.build();
+
+        OkHttpClient httpClient = new OkHttpClient.Builder().build();
+        Response response = httpClient.newCall(request).execute();
+
+        String responseBody = response.body().string();
+
+        System.out.println(responseBody);
+
+        return responseBody;
+    }
+
 
 }
--- src/main/java/it/gov/pagopa/tkm/ms/parretriever/client/amex/model/response/AmexParResponse.java
+++ src/main/java/it/gov/pagopa/tkm/ms/parretriever/client/amex/model/response/AmexParResponse.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class AmexParResponse {
 
     private String input;
