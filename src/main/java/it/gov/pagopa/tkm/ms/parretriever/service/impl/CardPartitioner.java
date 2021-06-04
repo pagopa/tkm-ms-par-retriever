893c844eaaa4f4cdbd7974d84e500d93aa27c078
andrea rovere <e-aarv@gft.com>
2021-06-04 15:38:20 +0200

    Pagopa-943 modified partitioner - limit number of call to circuit API

--- src/main/java/it/gov/pagopa/tkm/ms/parretriever/service/impl/CardPartitioner.java
+++ src/main/java/it/gov/pagopa/tkm/ms/parretriever/service/impl/CardPartitioner.java
@@ -1,7 +1,9 @@
 package it.gov.pagopa.tkm.ms.parretriever.service.impl;
 
+import com.google.common.util.concurrent.RateLimiter;
 import it.gov.pagopa.tkm.ms.parretriever.client.cards.*;
 import it.gov.pagopa.tkm.ms.parretriever.client.cards.model.response.*;
+import it.gov.pagopa.tkm.ms.parretriever.constant.CircuitEnum;
 import lombok.extern.log4j.*;
 import org.jetbrains.annotations.*;
 import org.springframework.batch.core.partition.support.Partitioner;
@@ -9,13 +11,32 @@ import org.springframework.batch.item.ExecutionContext;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 @Log4j2
 public class CardPartitioner implements Partitioner {
+
+
+    @Value("${batch-execution.amex-par-retrieve-enabled}")
+    private Boolean amexParRetrieveEnabled;
+
+    @Value("${batch-execution.mastercard-par-retrieve-enabled}")
+    private Boolean mastercardParRetrieveEnabled;
+
+    @Value("${batch-execution.visa-par-retrieve-enabled}")
+    private Boolean visaParRetrieveEnabled;
+
+    @Value("${batch-execution.amex-max-api-client-call-rate}")
+    private Double amexMaxApiClientCallRate;
+
+    @Value("${batch-execution.mastercard-max-api-client-call-rate}")
+    private Double mastercardMaxApiClientCallRate;
+
+    @Value("${batch-execution.visa-max-api-client-call-rate}")
+    private Double visaMaxApiClientCallRate;
 
     @Autowired
     private ParlessCardsClient parlessCardsClient;
@@ -23,37 +44,139 @@ public class CardPartitioner implements Partitioner {
     @NotNull
     @Override
     public Map<String, ExecutionContext> partition(@Value("${batch-execution.max-number-of-threads}") int maxNumberOfThreads) {
+
         Map<String, ExecutionContext> result = new HashMap<>();
 
         List<ParlessCard> cards = parlessCardsClient.getParlessCards(maxNumberOfThreads);
         int cardsSize = cards.size();
 
-        if (cardsSize < maxNumberOfThreads) {
-            maxNumberOfThreads = cardsSize;
+        //fallback
+        if (cardsSize == 0) {
+            return new HashMap<>();
         }
 
-        int range = cardsSize / maxNumberOfThreads;
-        int fromId = 1;
-        int toId = range;
+        //numero di carte per ogni circuito
+        EnumMap<CircuitEnum, List<ParlessCard>> parlessCardsPerCircuit = new EnumMap<>(CircuitEnum.class);
+        //tempo di elaborazione previsto per ogni circuito con thread singolo
+        EnumMap<CircuitEnum, Double> singleThreadElaborationTimePerCircuit = new EnumMap<>(CircuitEnum.class);
 
-        for (int i = 1; i <= maxNumberOfThreads; i++) {
-            ExecutionContext value = new ExecutionContext();
-            log.debug("\nStarting : Thread" + i + " from : " + fromId + " to : " + toId);
+        //Popolamento map circuito/carte
+        for (int i = 0; i < cardsSize; i++) {
+            CircuitEnum circuit = cards.get(i).getCircuit();
 
-            value.putInt("from", fromId);
-            value.putInt("to", toId);
-            value.putString("name", "Thread" + i);
-            value.put("cardList", cards.subList(fromId, Math.min(toId, cardsSize)));
+            if (!parlessCardsPerCircuit.containsKey(circuit)) {
+                List<ParlessCard> circuitCards = Stream.of(cards.get(i)).collect(Collectors.toList());
+                ;
+                parlessCardsPerCircuit.put(circuit, circuitCards);
+            } else {
+                parlessCardsPerCircuit.get(circuit).add(cards.get(i));
+            }
+        }
 
-            result.put("partition" + i, value);
+        //Popolamento map circuito/tempo di elaborazione per thread singolo
+        parlessCardsPerCircuit.keySet().forEach(c -> {
+            singleThreadElaborationTimePerCircuit.put(c, parlessCardsPerCircuit.get(c).size() /
+                    getApiCallMaxRateByCircuit(c));
+        });
 
-            fromId = toId + 1;
-            toId += range;
-            if (i == maxNumberOfThreads && cardsSize % maxNumberOfThreads > 0) {
-                toId += 1;
+        //Tempo totale di elaborazione
+        double totalElaborationTime =
+                singleThreadElaborationTimePerCircuit.values().stream().reduce(0d, Double::sum);
+
+        //Distribuzione del numero di thread tra i circuiti
+        Map<CircuitEnum, Integer> threadsPerCircuit = normalizeNumberOfThreads(singleThreadElaborationTimePerCircuit,
+                totalElaborationTime, maxNumberOfThreads);
+
+        //creazione degli execution context (thread) per ciascun circuito
+        for (CircuitEnum circuit : parlessCardsPerCircuit.keySet()) {
+
+            int cardSizeByCircuit = parlessCardsPerCircuit.get(circuit).size();
+            int maxNumberOfThreadsByCircuit = threadsPerCircuit.get(circuit);
+            int[] subListIndexes = subListIndexes(cardSizeByCircuit, maxNumberOfThreadsByCircuit);
+            ;
+
+            for (int i = 1; i < subListIndexes.length; i++) {
+                int fromId = subListIndexes[i - 1];
+                int toId = subListIndexes[i];
+
+                ExecutionContext value = new ExecutionContext();
+                log.debug("\nStarting : Thread" + i + " from : " + fromId + " to : " + toId);
+                double maxApiRatePerExecutionContext = getApiCallMaxRateByCircuit(circuit) / maxNumberOfThreadsByCircuit;
+
+                value.putInt("from", fromId);
+                value.putInt("to", toId);
+                value.putString("name", "Thread" + i);
+                value.put("cardList", cards.subList(fromId, Math.min(toId, cardsSize)));
+                value.put("rateLimiter", RateLimiter.create(Math.floor(maxApiRatePerExecutionContext)));
+                result.put("partition" + i, value);
+
             }
         }
         return result;
     }
 
+    private Double getApiCallMaxRateByCircuit(CircuitEnum circuit) {
+        switch (circuit) {
+            case AMEX:
+                return amexMaxApiClientCallRate;
+            case MASTERCARD:
+                return mastercardMaxApiClientCallRate;
+            case VISA:
+            case VISA_ELECTRON:
+            case VPAY:
+                return visaMaxApiClientCallRate;
+            default:
+                return 1d;
+        }
+
+    }
+
+    //Distribuzione del numero di thread in base al tempo previsto di elaborazione
+    private Map<CircuitEnum, Integer> normalizeNumberOfThreads(Map<CircuitEnum, Double> elaborationTimes,
+                                                               Double totalElaborationTime, int maxNumberOfThreads) {
+
+        //Assegnazione del numero di thread per ciascun circuito distribuendo secondo la proporzione
+        // numero_di_thread_circuito= (tempo_di_elaborazione_circuito/tempo_totale_elaborazione)*numero_max_thread
+        //valori arrotondati all'intero inferiore
+        EnumMap<CircuitEnum, Integer> threadsPerCircuit = new EnumMap<>(CircuitEnum.class);
+        for (Map.Entry<CircuitEnum, Double> entry : elaborationTimes.entrySet()) {
+            double doubleNumberOfThreads = (entry.getValue() / totalElaborationTime) * maxNumberOfThreads;
+            int downRoundedNumberOfThreads = (int) doubleNumberOfThreads;
+            threadsPerCircuit.put(entry.getKey(), downRoundedNumberOfThreads);
+        }
+
+        //ordinamento dei circuiti da quello che ha meno thread a quello che ne ha di più
+        Map<CircuitEnum, Integer> sortedThreadsPerCircuit = threadsPerCircuit.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.naturalOrder()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+        //aggiunta di un thread per circuito da quello che ne ha meno a quello che ne ha di più fino
+        //a che la somma dei thread è pari al numero massimo di thread
+        for (Map.Entry<CircuitEnum, Integer> entry : sortedThreadsPerCircuit.entrySet()) {
+            if (threadsPerCircuit.values().stream().reduce(0, Integer::sum) < maxNumberOfThreads) {
+                threadsPerCircuit.put(entry.getKey(), entry.getValue() + 1);
+            }
+        }
+
+        return threadsPerCircuit;
+    }
+
+    private int[] subListIndexes(Integer numberOfParlessCards, Integer maxNumberOfThread) {
+        int ratio = numberOfParlessCards / maxNumberOfThread;
+        int subListCoveredByRatio = ratio * maxNumberOfThread;
+        int[] subListIndexes = new int[maxNumberOfThread + 1];
+        subListIndexes[0] = 0;
+        int remaining = numberOfParlessCards - subListCoveredByRatio;
+
+        for (int i = 1; i < remaining + 1; i++) {
+            subListIndexes[i] = subListIndexes[i - 1] + ratio + 1;
+        }
+
+        for (int i = remaining + 1; i < maxNumberOfThread + 1; i++) {
+            subListIndexes[i] = subListIndexes[i - 1] + ratio;
+        }
+        return subListIndexes;
+
+    }
 }
--- src/main/java/it/gov/pagopa/tkm/ms/parretriever/service/impl/CardWriter.java
+++ src/main/java/it/gov/pagopa/tkm/ms/parretriever/service/impl/CardWriter.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.tkm.ms.parretriever.service.impl;
 
 import com.fasterxml.jackson.databind.*;
+import com.google.common.util.concurrent.RateLimiter;
 import it.gov.pagopa.tkm.ms.parretriever.client.amex.*;
 import it.gov.pagopa.tkm.ms.parretriever.client.cards.model.response.*;
 import it.gov.pagopa.tkm.ms.parretriever.client.mastercard.*;
@@ -8,11 +9,14 @@ import it.gov.pagopa.tkm.ms.parretriever.client.visa.*;
 import it.gov.pagopa.tkm.ms.parretriever.constant.*;
 import it.gov.pagopa.tkm.ms.parretriever.model.topic.*;
 import org.jetbrains.annotations.*;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.*;
 import java.util.stream.*;
 
@@ -34,6 +38,30 @@ public class CardWriter implements ItemWriter<ParlessCard> {
 
     @Autowired
     private ObjectMapper mapper;
+
+    @Value("${batch-execution.amex-par-retrieve-enabled}")
+    private Boolean amexParRetrieveEnabled;
+
+    @Value("${batch-execution.mastercard-par-retrieve-enabled}")
+    private Boolean mastercardParRetrieveEnabled;
+
+    @Value("${batch-execution.visa-par-retrieve-enabled}")
+    private Boolean visaParRetrieveEnabled;
+
+    @Value("${batch-execution.amex-max-api-client-call-rate}")
+    private Double amexMaxApiClientCallRate;
+
+    @Value("${batch-execution.mastercard-max-api-client-call-rate}")
+    private Double mastercardMaxApiClientCallRate;
+
+    @Value("${batch-execution.visa-max-api-client-call-rate}")
+    private Double visaMaxApiClientCallRate;
+
+    RateLimiter amexRateLimiter = RateLimiter.create(amexMaxApiClientCallRate);
+    RateLimiter mastercardRateLimiter = RateLimiter.create(mastercardMaxApiClientCallRate);
+    RateLimiter visaRateLimiter = RateLimiter.create(visaMaxApiClientCallRate);
+
+    private StepExecution stepExecution;
 
     @Override
     public void write(@NotNull List<? extends ParlessCard> list) throws Exception {
@@ -58,8 +86,14 @@ public class CardWriter implements ItemWriter<ParlessCard> {
     }
 
     private void writeCardsOnKafkaQueue(List<? extends ParlessCard> parlessCardResponseList) throws Exception {
+        ExecutionContext stepContext = this.stepExecution.getExecutionContext();
+        RateLimiter rateLimiter = (RateLimiter)stepContext.get("rateLimiter");
+        rateLimiter.acquire(1);
+
         for (ParlessCard parlessCard : parlessCardResponseList) {
             CircuitEnum circuit = parlessCard.getCircuit();
+            if (!checkParRetrieveEnabledAndRateLimitByCircuit(circuit)) continue;
+
             String par = getParFromCircuit(circuit, parlessCard.getPan());
             ReadQueue readQueue = new ReadQueue(parlessCard.getTaxCode(),
                     parlessCard.getPan(),
@@ -74,5 +108,26 @@ public class CardWriter implements ItemWriter<ParlessCard> {
     private List<Token> getTokenListFromStringSet(Set<String> tokens) {
         return tokens.stream().map(z -> new Token(z, null)).collect(Collectors.toList());
     }
+
+    private boolean checkParRetrieveEnabledAndRateLimitByCircuit(CircuitEnum circuit) {
+        switch (circuit) {
+            case AMEX:
+                return amexParRetrieveEnabled && acquireLimiterSlot(amexRateLimiter);
+            case MASTERCARD:
+                return mastercardParRetrieveEnabled && acquireLimiterSlot(mastercardRateLimiter);
+            case VISA:
+            case VISA_ELECTRON:
+            case VPAY:
+                return visaParRetrieveEnabled && acquireLimiterSlot(visaRateLimiter);
+            default:
+                return false;
+        }
+
+    }
+
+    private Boolean acquireLimiterSlot(RateLimiter limiter){
+        limiter.acquire(1);
+        return true;
+  }
 
 }
