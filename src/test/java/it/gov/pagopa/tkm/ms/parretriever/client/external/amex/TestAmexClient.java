df5bd53e61234e2c5f3e8b7cb831661c30a2ef43
David Bertoni <David Bertoni>
2021-06-07 15:55:15 +0200

    PagoPA-943 - Amex test

--- src/test/java/it/gov/pagopa/tkm/ms/parretriever/client/external/amex/TestAmexClient.java
+++ src/test/java/it/gov/pagopa/tkm/ms/parretriever/client/external/amex/TestAmexClient.java
@@ -1,11 +1,20 @@
 package it.gov.pagopa.tkm.ms.parretriever.client.external.amex;
 
 import com.fasterxml.jackson.databind.*;
+import it.gov.pagopa.tkm.ms.parretriever.client.external.amex.model.response.*;
 import it.gov.pagopa.tkm.ms.parretriever.constant.*;
+import okhttp3.mockwebserver.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.*;
 import org.mockito.*;
 import org.mockito.junit.jupiter.*;
+import org.springframework.test.util.*;
+
+import java.io.*;
+import java.lang.reflect.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
@@ -14,15 +23,50 @@ public class TestAmexClient {
     @InjectMocks
     private AmexClient amexClient;
 
-    private DefaultBeans testBeans;
+    @Mock
+    private ObjectMapper mockMapper;
 
     private final ObjectMapper mapper = new ObjectMapper();
 
+    private DefaultBeans testBeans;
+
+    private static MockWebServer mockServer;
+
     @BeforeEach
-    void init() {
+    void init() throws Exception {
         testBeans = new DefaultBeans();
+        AmexClient.class.getDeclaredField("clientId").setAccessible(true);
+        AmexClient.class.getDeclaredField("clientSecret").setAccessible(true);
+        AmexClient.class.getDeclaredField("retrieveParUrl").setAccessible(true);
+        ReflectionTestUtils.setField(amexClient, "clientId", "TEST_ID");
+        ReflectionTestUtils.setField(amexClient, "clientSecret", "TEST_SECRET");
+        ReflectionTestUtils.setField(amexClient, "retrieveParUrl", "http://localhost:" + mockServer.getPort());
+        Method postConstruct = AmexClient.class.getDeclaredMethod("init");
+        postConstruct.setAccessible(true);
+        postConstruct.invoke(amexClient);
     }
 
-    //TODO TESTS
+    @BeforeAll
+    static void setUp() throws IOException {
+        mockServer = new MockWebServer();
+        mockServer.start();
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        mockServer.shutdown();
+    }
+
+    @Test
+    void givenPan_assertApiGetsCalled() throws IOException {
+        String responseAsString = mapper.writeValueAsString(testBeans.AMEX_PAR_RESPONSE);
+        MockResponse mockResponse = new MockResponse()
+                .setBody(responseAsString)
+                .addHeader("Content-Type", "application/json");
+        mockServer.enqueue(mockResponse);
+        when(mockMapper.readValue(responseAsString, AmexParResponse[].class)).thenReturn(mapper.readValue(responseAsString, AmexParResponse[].class));
+        String actualPar = amexClient.getPar(testBeans.PAN);
+        assertEquals(testBeans.PAR, actualPar);
+    }
 
 }
--- src/test/java/it/gov/pagopa/tkm/ms/parretriever/constant/DefaultBeans.java
+++ src/test/java/it/gov/pagopa/tkm/ms/parretriever/constant/DefaultBeans.java
@@ -1,4 +1,12 @@
 package it.gov.pagopa.tkm.ms.parretriever.constant;
 
+import it.gov.pagopa.tkm.ms.parretriever.client.external.amex.model.response.*;
+
 public class DefaultBeans {
+
+    public final String PAN = "111111111111";
+    public final String PAR = "abc11111111111";
+
+    public final AmexParResponse[] AMEX_PAR_RESPONSE = new AmexParResponse[]{new AmexParResponse(PAN, PAR, true)};
+
 }
