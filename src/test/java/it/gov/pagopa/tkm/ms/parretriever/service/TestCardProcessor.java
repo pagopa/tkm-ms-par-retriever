7912812fff89c71bdeb7dec041e1aa905c5c6b58
a99z <ignazio.maniscalco@gft.com>
2021-06-17 12:04:49 +0200

    PAGOPA-943 - Tests for Processor and Writer

--- src/test/java/it/gov/pagopa/tkm/ms/parretriever/service/TestCardProcessor.java
+++ src/test/java/it/gov/pagopa/tkm/ms/parretriever/service/TestCardProcessor.java
@@ -1,6 +1,7 @@
 package it.gov.pagopa.tkm.ms.parretriever.service;
 
 import it.gov.pagopa.tkm.ms.parretriever.client.internal.cardmanager.*;
+import it.gov.pagopa.tkm.ms.parretriever.client.internal.cardmanager.model.response.ParlessCard;
 import it.gov.pagopa.tkm.ms.parretriever.client.internal.consentmanager.*;
 import it.gov.pagopa.tkm.ms.parretriever.constant.*;
 import it.gov.pagopa.tkm.ms.parretriever.service.impl.*;
@@ -8,6 +9,9 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.*;
 import org.mockito.*;
 import org.mockito.junit.jupiter.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
@@ -29,6 +33,20 @@ public class TestCardProcessor {
         testBeans = new DefaultBeans();
     }
 
-    //TODO TESTS
+    @Test
+    void processParlessCard_process() {
+        cardProcessor = new CardProcessor();
+        for (ParlessCard card: testBeans.PARLESS_CARDS_LIST) {
+            ParlessCard processedCard = cardProcessor.process(card);
+            assertEquals(card, processedCard);
+        }
+    }
+
+    @Test
+    void givenEmptyCard_returnNull() {
+        cardProcessor = new CardProcessor();
+        ParlessCard card = cardProcessor.process(new ParlessCard());
+        assertNull(card);
+    }
 
 }
--- src/test/java/it/gov/pagopa/tkm/ms/parretriever/service/TestCardWriter.java
+++ src/test/java/it/gov/pagopa/tkm/ms/parretriever/service/TestCardWriter.java
@@ -4,12 +4,17 @@ import com.fasterxml.jackson.databind.*;
 import it.gov.pagopa.tkm.ms.parretriever.client.external.amex.*;
 import it.gov.pagopa.tkm.ms.parretriever.client.external.mastercard.*;
 import it.gov.pagopa.tkm.ms.parretriever.client.external.visa.*;
+import it.gov.pagopa.tkm.ms.parretriever.client.internal.cardmanager.model.response.ParlessCard;
 import it.gov.pagopa.tkm.ms.parretriever.constant.*;
 import it.gov.pagopa.tkm.ms.parretriever.service.impl.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.*;
 import org.mockito.*;
 import org.mockito.junit.jupiter.*;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
@@ -39,6 +44,11 @@ public class TestCardWriter {
         testBeans = new DefaultBeans();
     }
 
-    //TODO TESTS
+    @Test
+    void writeOnQueue_exceptionCheck() {
+        List<ParlessCard> parlessCards = testBeans.PARLESS_CARDS_LIST;
+        cardWriter = new CardWriter(parlessCards, (double) parlessCards.size());
+        assertDoesNotThrow(() -> cardWriter.write(parlessCards));
+    }
 
 }
