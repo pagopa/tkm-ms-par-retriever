efea48692cee86702dd926936633762f623e4dc6
a99z <ignazio.maniscalco@gft.com>
2021-06-25 16:22:25 +0200

    PAGOPA-943 - Added Customized Kafka Config for Running Pipelines in SIT Environment

--- src/main/java/it/gov/pagopa/tkm/ms/parretriever/config/KafkaConfig.java
+++ src/main/java/it/gov/pagopa/tkm/ms/parretriever/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package it.gov.pagopa.tkm.ms.parretriever.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,11 +16,14 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
